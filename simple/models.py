38970a86736e378ec4993ea7b7f0c02e1a2cc7be
muhammadjon14114 <muhammadjonxamidov593@gmail.com>
2025-10-20 16:10:23 +0500

    commit

--- simple/models.py
+++ simple/models.py
@@ -6,8 +6,8 @@ from django.db import models
 class CustomUser(AbstractUser):
     # Qo‘shimcha maydonlar istasangiz shu yerga yozasiz
     # masalan: phone = models.CharField(max_length=20, blank=True)
-    user_name = models.CharField(max_length=30, blank=True)
-    password = models.CharField(max_length=10, blank=True)
-
-    def __str__(self):
-        return self.username
+    #user_name = models.CharField(max_length=30, blank=True)
+    #password = models.CharField(max_length=10, blank=True)
+    pass
+#    def __str__(self):
+#        return self.username
