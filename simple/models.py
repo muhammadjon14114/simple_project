acf07c2f79bbee36bece3e1e2ef3202621de8c54
muhammadjon14114 <muhammadjonxamidov593@gmail.com>
2025-10-20 16:14:04 +0500

    commit

--- simple/models.py
+++ simple/models.py
@@ -1,13 +1,20 @@
-# simple/models.py
-
-from django.contrib.auth.models import AbstractUser
 from django.db import models
 
-class CustomUser(AbstractUser):
-    # Qo‘shimcha maydonlar istasangiz shu yerga yozasiz
-    # masalan: phone = models.CharField(max_length=20, blank=True)
-    #user_name = models.CharField(max_length=30, blank=True)
-    #password = models.CharField(max_length=10, blank=True)
-    pass
-#    def __str__(self):
-#        return self.username
+
+class User(models.Model):
+    username = models.CharField(max_length=50, unique=True)
+    name = models.CharField(max_length=100)
+    email = models.EmailField(unique=True)
+    phone = models.CharField(max_length=15, blank=True, null=True)
+    address = models.CharField(max_length=255, blank=True, null=True)
+    password = models.CharField(max_length=128)
+    date_joined = models.DateTimeField(auto_now_add=True)
+    is_active = models.BooleanField(default=True)
+
+    class Meta:
+        db_table = 'user'  # Bazadagi jadval nomi
+        verbose_name = 'User'
+        verbose_name_plural = 'Users'
+
+    def __str__(self):
+        return f"{self.username} ({self.email})"
